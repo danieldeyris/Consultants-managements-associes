795d8b8f1988d220d74a7f4085eb9054081daf9c
Vincent GIRARD <vig@ABEILLEAIMASSO.LOCAL>
2022-03-29 10:09:30 +0200

    ACOMPTE - Mise en place structure

---
+++ abei_acompte/models/sale_order.py
@@ -0,0 +1,27 @@
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    # Une case à cocher permet d’indiquer que la commande est soumise à acompte
+    acompte_checkbox = fields.Boolean(string="Géré par acompte")
+
+    # Le type d’acompte permet d’indiquer la fréquence d’émission des factures d’acompte
+    # Mensuel (tous les mois)
+    # Bimestriel (tous les 2 mois)
+    # Trimestriel (tous les 3 mois)
+    # Semestriel (tous les 6 mois)
+    acompte_type = fields.Selection([('mensuel','Mensuel'),
+                                     ('bimestriel','Bimestriel'),
+                                     ('trimestriel','Trimestriel'),
+                                     ('semestriel','Semestriel')])
+
+    acompte_date_debut = fields.Date()
+
+    # def _prepare_invoice(self):
+    #     res = super()._prepare_invoice()
+    #     res['acompte_checkbox'] = self.acompte_checkbox
+    #     res['acompte_type'] = self.acompte_type
+    #     res['acompte_date_debut'] = self.acompte_date_debut
+    #     return res
