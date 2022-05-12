756448fcda8986fa462690514281543808f19b69
Vincent GIRARD <vig@ABEILLEAIMASSO.LOCAL>
2022-05-12 12:43:19 +0200

    FEUILLES DE TEMPS : RETRAIT CHAMP client, UTILISATION CHAMP ODOO partner_id

--- abei_feuille_temps/models/analytic_account.py
+++ abei_feuille_temps/models/analytic_account.py
@@ -4,4 +4,5 @@ from odoo import models, fields
 class AnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
-    client = fields.Many2one('res.partner', string="Client", required=True, domain=['|', ('is_company', '=', True), ('parent_id', '=', False)])
+    partner_id = fields.Many2one('res.partner', string='Client', check_company=True, required=True)
+
