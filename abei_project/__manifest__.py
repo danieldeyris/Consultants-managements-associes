c32dc1c40fb07607aece5c8a75d863916bd46145
Vincent GIRARD <vig@ABEILLEAIMASSO.LOCAL>
2022-05-19 11:43:30 +0200

    ABEI_FEUILLE_TEMPS: AJOUT NOMBRE BULLETINS DANS SAISIE DES TEMPS, ABEI_PROJECT: VERIFICATION TRANSFERT DE TACHE D'UNE ETAPE A UNE AUTRE. SI ETAPE FLAGGEE COMME NECESS ITANT UNE SAISIE DE TEMPS OU BULLETIN, VERIFICATION VALIDITE , SINON, REFUS DE TRANSFERT

--- abei_project/__manifest__.py
+++ abei_project/__manifest__.py
@@ -16,11 +16,16 @@ Ajout de divers filtre dans vues project.project et project.task :
 - Informatique
 
 - Ajout champ "Etiquettes de projet" dans la vue formulaire project.project
+
+Dans le projet, vérification transfert de tâche d'une étape à une autre étape.
+Si étape flaggée comme necessitant une saisie de temps ou de bulletins (module abei_project_stage_timesheet) vérification validité, sinon, refus de transfert
 """,
     'author': 'Abeille',
     'website': 'https://aca-consult.com/',
     'depends': [
         'project',
+        'abei_project_stage_timesheet',
+        'abei_feuille_temps',
     ],
     'data': [
         'views/project_project.xml',
