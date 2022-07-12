b4d5b0bc8eccd1625856d6c083f11b04adc3ce36
Vincent GIRARD <vig@ABEILLEAIMASSO.LOCAL>
2022-07-12 17:30:00 +0200

    AJOUTE MODULE SIGNATURE MULTI SOCIETE

---
+++ abei_signature/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Abeille : Signature',
+    'version': '14.0.0.0',
+    'summary': 'Signature',
+    'description': """
+    Ajout d'un onglet "Signature email" dans la vue user pour permettre l'ajout de plusieures signatures multi compagnie
+""",
+    'author': 'Abeille',
+    'website': 'https://aca-consult.com/',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_user_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+    'license': 'OEEL-1',
+}
